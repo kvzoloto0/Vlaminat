a6d0f70fec6d7e8fbcbf9f8082f73549244ae02b
kvzoloto <kirill2000zol@gmail.com>
2021-02-06 19:56:45 +0200

    New Commits

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+$("#navToggle").click(function() {                                          /*navBurger*/
+    $(this).toggleClass("active");
+    $(".overlay").toggleClass("open");
+    // this line ▼ prevents content scroll-behind
+    $("body").toggleClass("locked");
+});
+$('.overlay').click(function() {
+    $(this).removeClass('open');
+    $('.navBurger').removeClass('active');
+});    
+
+
 $(function(){
   $('.minimized').click(function(event) { 
     var i_path = $(this).attr('src');
