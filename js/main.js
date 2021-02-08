7dedcedb969f7518b89692b4564f22de053ad13c
kvzoloto <kirill2000zol@gmail.com>
2021-02-08 02:21:36 +0200

    nightCommits

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 $("#navToggle").click(function() {                                          /*navBurger*/
     $(this).toggleClass("active");
     $(".overlay").toggleClass("open");
-    // this line â–¼ prevents content scroll-behind
+    // this line ¡ prevents content scroll-behind
     $("body").toggleClass("locked");
 });
 $('.overlay').click(function() {
     $(this).removeClass('open');
     $('.navBurger').removeClass('active');
-});    
+});                                                                            /*navBurger*/
 
 
 $(function(){
@@ -28,3 +28,37 @@ $(function(){
     });
   });
 });   
+
+
+
+
+
+var element = document.getElementById('phone');
+var maskOptions = {
+    mask: '+38(000)000-00-00',
+    lazy: false
+} 
+var mask = new IMask(element, maskOptions);
+
+var element2 = document.getElementById('email');
+var maskOptions2 = {    
+    mask:function (value) {
+                if(/^[a-z0-9_\.-]+$/.test(value))
+                    return true;
+                if(/^[a-z0-9_\.-]+@$/.test(value))
+                    return true;
+                if(/^[a-z0-9_\.-]+@[a-z0-9-]+$/.test(value))
+                    return true;
+                if(/^[a-z0-9_\.-]+@[a-z0-9-]+\.$/.test(value))
+                    return true;
+                if(/^[a-z0-9_\.-]+@[a-z0-9-]+\.[a-z]{1,4}$/.test(value))
+                    return true;
+                if(/^[a-z0-9_\.-]+@[a-z0-9-]+\.[a-z]{1,4}\.$/.test(value))
+                    return true;
+                if(/^[a-z0-9_\.-]+@[a-z0-9-]+\.[a-z]{1,4}\.[a-z]{1,4}$/.test(value))
+                    return true;
+                return false;
+                    },
+    lazy: false
+} 
+var mask2 = new IMask(element2, maskOptions2);
