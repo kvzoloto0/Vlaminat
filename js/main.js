a1184fe6ae3a2aed919d4e5c037c016fda2a5d17
kvzoloto0 <69841296+kvzoloto0@users.noreply.github.com>
2021-02-14 05:28:42 +0200

    popupmenu

--- js/main.js
+++ js/main.js
@@ -16,34 +16,36 @@ $('.overlay').click(function() {
 
 
 var element = document.getElementById('phone');
+
 var maskOptions = {
     mask: '+38(000)000-00-00',
-    lazy: false
+    lazy: false,
 } 
+
 var mask = new IMask(element, maskOptions);
 
-var element2 = document.getElementById('email');
-var maskOptions2 = {    
-    mask:function (value) {
-                if(/^[a-z0-9_\.-]+$/.test(value))
-                    return true;
-                if(/^[a-z0-9_\.-]+@$/.test(value))
-                    return true;
-                if(/^[a-z0-9_\.-]+@[a-z0-9-]+$/.test(value))
-                    return true;
-                if(/^[a-z0-9_\.-]+@[a-z0-9-]+\.$/.test(value))
-                    return true;
-                if(/^[a-z0-9_\.-]+@[a-z0-9-]+\.[a-z]{1,4}$/.test(value))
-                    return true;
-                if(/^[a-z0-9_\.-]+@[a-z0-9-]+\.[a-z]{1,4}\.$/.test(value))
-                    return true;
-                if(/^[a-z0-9_\.-]+@[a-z0-9-]+\.[a-z]{1,4}\.[a-z]{1,4}$/.test(value))
-                    return true;
-                return false;
-                    },
-    lazy: false
-} 
-var mask2 = new IMask(element2, maskOptions2);
+//var element2 = document.getElementById('email');
+//var maskOptions2 = {    
+//    mask:function (value) {
+//                if(/^[a-z0-9_\.-]+$/.test(value))
+//                    return true;
+//                if(/^[a-z0-9_\.-]+@$/.test(value))
+//                    return true;
+//                if(/^[a-z0-9_\.-]+@[a-z0-9-]+$/.test(value))
+//                    return true;
+//                if(/^[a-z0-9_\.-]+@[a-z0-9-]+\.$/.test(value))
+//                    return true;
+//                if(/^[a-z0-9_\.-]+@[a-z0-9-]+\.[a-z]{1,4}$/.test(value))
+//                    return true;
+//                if(/^[a-z0-9_\.-]+@[a-z0-9-]+\.[a-z]{1,4}\.$/.test(value))
+//                    return true;
+//                if(/^[a-z0-9_\.-]+@[a-z0-9-]+\.[a-z]{1,4}\.[a-z]{1,4}$/.test(value))
+//                    return true;
+//                return false;
+//                    },
+//    lazy: false
+//} 
+//var mask2 = new IMask(element2, maskOptions2);
 
 
 
